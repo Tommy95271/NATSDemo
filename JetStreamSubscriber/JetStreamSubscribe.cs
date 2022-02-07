588c8216b9daa119d5b34c47deee080a9819c4a9
謝志斌 Tommy <tommy@miniasp.com>
2022-02-07 17:31:24 +0800

    JetStream in console demo modification.

--- JetStreamSubscriber/JetStreamSubscribe.cs
+++ JetStreamSubscriber/JetStreamSubscribe.cs
@@ -13,6 +13,7 @@ namespace JetStreamSubscriber
     {
         private static IConnection? _connection;
         private const string ALLOWED_OPTIONS = "1234qQ";
+        private static string[] _invalidChar = new string[] { " ", ".", ">", "*" };
         private static bool exit = false;
         private static IJetStreamManagement jsm { get; set; }
 
@@ -25,13 +26,25 @@ namespace JetStreamSubscriber
                 {
                     Console.Clear();
 
+                    var modes = new List<string>() {
+                        "JetStream list all streams",
+                        "JetStream list all consumers in a stream",
+                        "JetStream create stream",
+                        "JetStream create consumer",
+                        "JetStream subscribe a subject from stream (pull-based)",
+                        "JetStream delete stream",
+                        "JetStream delete consumer" };
+
                     Console.WriteLine("NATS JetStream demo consumer");
                     Console.WriteLine("==================");
                     Console.WriteLine("Select mode:");
-                    Console.WriteLine("1) JetStream create stream");
-                    Console.WriteLine("2) JetStream create consumer");
-                    Console.WriteLine("3) JetStream subscribe a subject from stream");
-                    Console.WriteLine("4) JetStream delete consumer");
+
+                    int count = 1;
+                    foreach (var mode in modes)
+                    {
+                        Console.WriteLine($"{count}) {mode}");
+                        count++;
+                    }
                     Console.WriteLine("q) Quit");
 
                     string input;
@@ -43,15 +56,24 @@ namespace JetStreamSubscriber
                     switch (input)
                     {
                         case "1":
-                            JetStreamCreateStream();
+                            JetStreamListStreams();
                             break;
                         case "2":
-                            JetStreamCreateConsumer();
+                            JetStreamListConsumers();
                             break;
                         case "3":
-                            JetStreamSub();
+                            JetStreamCreateStream();
                             break;
                         case "4":
+                            JetStreamCreateConsumer();
+                            break;
+                        case "5":
+                            JetStreamSubPullBased();
+                            break;
+                        case "6":
+                            JetStreamDeleteStream();
+                            break;
+                        case "7":
                             JetStreamDeleteConsumer();
                             break;
                         case "q":
@@ -79,9 +101,45 @@ namespace JetStreamSubscriber
             return factory.CreateConnection(options);
         }
 
+        private static void JetStreamListStreams()
+        {
+            banner("JetStream list all stream demo");
+            var streamNames = jsm.GetStreamNames();
+            var count = 1;
+            foreach (var streamName in streamNames)
+            {
+                Console.WriteLine($"{count}) {streamName}");
+                count++;
+            }
+        }
+
+        private static void JetStreamListConsumers()
+        {
+            banner("JetStream list all consumers demo");
+            var streamResult = streamExists();
+            if (streamResult.result)
+            {
+                var consumerNames = jsm.GetConsumerNames(streamResult.streamName);
+                if (consumerNames.Count == 0)
+                {
+                    Console.WriteLine("There is no consumer in the stream.");
+                }
+                else
+                {
+                    Console.WriteLine("Below are consumers in the stream.");
+                    var count = 1;
+                    foreach (var consumerName in consumerNames)
+                    {
+                        Console.WriteLine($"{count}) {consumerName}");
+                        count++;
+                    }
+                }
+            }
+        }
+
         private static void JetStreamCreateStream()
         {
-            banner("JetStream Create Stream demo");
+            banner("JetStream create stream demo");
 
             var streamResult = streamExistsBeforeCreating();
             if (streamResult.result)
@@ -105,7 +163,7 @@ namespace JetStreamSubscriber
 
         private static void JetStreamCreateConsumer()
         {
-            banner("JetStream Create Consumer demo");
+            banner("JetStream create consumer demo");
 
             var streamResult = streamExists();
 
@@ -113,9 +171,9 @@ namespace JetStreamSubscriber
             {
                 Console.WriteLine("Please type in a consumer name.");
                 var consumer = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(consumer))
+                if (string.IsNullOrWhiteSpace(consumer) || _invalidChar.Any(i => consumer.Contains(i)))
                 {
-                    Console.WriteLine("Please type in a valid consumer name.");
+                    Console.WriteLine("Space, period, > or * is not allowed, please type in a valid consumer name.");
                 }
                 else
                 {
@@ -124,9 +182,9 @@ namespace JetStreamSubscriber
             }
         }
 
-        private static void JetStreamSub()
+        private static void JetStreamSubPullBased()
         {
-            banner("JetStream Subscribe demo");
+            banner("JetStream subscribe demo");
             var streamResult = streamExists();
             var consumerResult = consumerExists(streamResult.streamName);
             if (streamResult.result && consumerResult.result)
@@ -148,17 +206,24 @@ namespace JetStreamSubscriber
                 if (regex.IsMatch(count))
                 {
                     var countInt = int.Parse(count);
-                    IList<Msg> list = sub.Fetch(countInt, 1000);
-                    if (countInt > list.Count)
+                    if (countInt == 0)
                     {
-                        Console.WriteLine($"The count in subject: {subjectResult.subjectName} is {list.Count}, please type in smaller number.");
+                        Console.WriteLine("0 message is not allowed!");
                     }
                     else
                     {
-                        for (int i = 0; i < list.Count; i++)
+                        IList<Msg> list = sub.Fetch(countInt, 1000);
+                        if (countInt > list.Count)
                         {
-                            Console.WriteLine($"{i}. Message: {list[i]}");
-                            list[i].Ack();
+                            Console.WriteLine($"The count in subject: {subjectResult.subjectName} is {list.Count}, please type in smaller number.");
+                        }
+                        else
+                        {
+                            for (int i = 0; i < list.Count; i++)
+                            {
+                                Console.WriteLine($"{i}. Message: {list[i]}");
+                                list[i].Ack();
+                            }
                         }
                     }
                 }
@@ -166,6 +231,16 @@ namespace JetStreamSubscriber
                 {
                     Console.WriteLine("Please type in digitals from 0 to 9.");
                 }
+            }
+        }
+
+        private static void JetStreamDeleteStream()
+        {
+            banner("JetStream delete stream demo");
+            var streamResult = streamExists();
+            if (streamResult.result)
+            {
+                jsm.DeleteStream(streamResult.streamName);
             }
         }
 
@@ -374,9 +449,9 @@ namespace JetStreamSubscriber
         {
             Console.WriteLine("Please type in a stream name.");
             var streamName = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(streamName))
+            if (string.IsNullOrWhiteSpace(streamName) || _invalidChar.Any(i => streamName.Contains(i)))
             {
-                Console.WriteLine("Please type in a valid stream name.");
+                Console.WriteLine("Space, period, > or * is not allowed, please type in a valid stream name.");
                 return (false, null);
             }
             var streamNames = jsm.GetStreamNames();
